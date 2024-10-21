f3f84de00a43c420f4a0d97e53b294943a82cce9
Max Wang <max@covar.com>
2024-10-21 09:57:05 -0400

    Add ARA and ARS tests

--- test_generators/performance_suite_generator.py
+++ test_generators/performance_suite_generator.py
@@ -34,11 +34,45 @@ def generate_message(query, num_curies):
     return query
 
 
+creative_query = {
+    "message": {
+        "query_graph": {
+            "nodes": {
+                "ON": {
+                    "categories": [
+                        "biolink:Disease"
+                    ],
+                    "ids": [
+                        "MONDO:0005301"
+                    ]
+                },
+                "SN": {
+                    "categories": [
+                        "biolink:ChemicalEntity"
+                    ]
+                }
+            },
+            "edges": {
+                "t_edge": {
+                    "object": "ON",
+                    "subject": "SN",
+                    "predicates": [
+                        "biolink:treats"
+                    ],
+                    "knowledge_type": "inferred"
+                }
+            }
+        }
+    },
+    "bypass_cache": True,
+}
+
+
 def create_test_suite(
     logger: logging.Logger,
 ) -> None:
     """Download tests from specified location."""
-    with open("./asset_backups/performance_tests_2024_10_18.json", "r") as f:
+    with open("./asset_backups/kp_performance_tests_2024_10_18.json", "r") as f:
         data = json.load(f)
 
         test_suite = {
@@ -52,7 +86,7 @@ def create_test_suite(
             "test_suite_specification": None,
             "test_cases": {},
         }
-        # sequential
+        # sequential KP
         for rate in [(900, 1), (180, 10), (90, 100), (30, 1000)]:
             for kp in data.values():
                 infores = kp["infores"]
@@ -69,7 +103,7 @@ def create_test_suite(
                     "components": [infores],
                 }
                 test_suite["test_cases"][uid] = test_case
-        # concurrent
+        # concurrent KP
         for rate in [(10, 1), (100, 1), (1000, 1), (10, 10), (100, 10), (1000, 10), (10, 1000), (100, 1000), (1000, 1000)]:
             for kp in data.values():
                 infores = kp["infores"]
@@ -86,6 +120,62 @@ def create_test_suite(
                     "components": [infores],
                 }
                 test_suite["test_cases"][uid] = test_case
+        
+        # sequential ARA
+        test_suite["test_cases"]["ara_sequential_tests"] = {
+            "id": "ara_sequential_tests",
+            "name": "Sequential ARA Performance Tests",
+            "description": "Sequential ARA Performance Tests",
+            "tags": [],
+            "test_runner_settings": [],
+            "query": creative_query,
+            "num_queries": 15,
+            "concurrent": False,
+            "components": ["ara"],
+        }
+
+        # concurrent ARA
+        for rate in [10, 50, 100, 1000]:
+            uid = f"ara_{rate}_concurrent_tests"
+            test_suite["test_cases"][uid] = {
+                "id": uid,
+                "name": f"{rate} Concurrent ARA Performance Tests",
+                "description": f"{rate} Concurrent ARA Performance Tests",
+                "tags": [],
+                "test_runner_settings": [],
+                "query": creative_query,
+                "num_queries": rate,
+                "concurrent": True,
+                "components": ["ara"],
+            }
+
+        # sequential ARS
+        test_suite["test_cases"]["ars_sequential_tests"] = {
+            "id": "ars_sequential_tests",
+            "name": "Sequential ARS Performance Tests",
+            "description": "Sequential ARS Performance Tests",
+            "tags": [],
+            "test_runner_settings": [],
+            "query": creative_query,
+            "num_queries": 15,
+            "concurrent": False,
+            "components": ["ars"],
+        }
+
+        # concurrent ARS
+        for rate in [10, 50, 100, 1000]:
+            uid = f"ars_{rate}_concurrent_tests"
+            test_suite["test_cases"][uid] = {
+                "id": uid,
+                "name": "Concurrent ARS Performance Tests",
+                "description": "Concurrent ARS Performance Tests",
+                "tags": [],
+                "test_runner_settings": [],
+                "query": creative_query,
+                "num_queries": rate,
+                "concurrent": True,
+                "components": ["ars"],
+            }
         
         with open("../test_suites/performance_tests.json", "w") as f:
             json.dump(test_suite, f, indent=2)
